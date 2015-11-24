00f815d3835106a5ecc88801b9630c462d5f7a7a
Dom Batten <dominic.batten@googlemail.com>
2015-11-24 16:03:11 +0000

    four tests passing, search factory service added

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,7 +1,15 @@
 gifsplashSearch.factory('Search', ['$http', function($http) {
-
+  var queryUrl = 'http://api.giphy.com/v1/gifs/search';
   return {
-    query: 'test'
+    query: function(searchTerm) {
+      return $http({
+        url: queryUrl,
+        method: 'GET',
+        params: {
+          'q': searchTerm,
+          'api_key': 'dc6zaTOxFJmzC'
+        }
+      });
+    }
   }
-
 }]);
--- test/searchfactory.spec.js
+++ test/searchfactory.spec.js
@@ -8,8 +8,35 @@ describe('factory: Search', function() {
     search = Search;
   }));
 
+  var sampleGif = [
+    {
+      "type": "gif",
+      "id": "FiGiRei2ICzzG"
+    }
+  ];
+
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend
+    httpBackend
+      .when("GET", "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=hello")
+      .respond(
+        [
+          { "type": "gif",
+            "id": "FiGiRei2ICzzG"
+          }
+        ]
+      );
+  }));
+
   it('responds to query', function() {
     expect(search.query).toBeDefined();
   });
 
+  it('returns search results', function() {
+    search.query('hello')
+      .then(function(response) {
+        expect(response.data).toEqual(sampleGif)
+      })
+    httpBackend.flush();
+  });
 });
